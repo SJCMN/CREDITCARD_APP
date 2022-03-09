8d56d8b5d1d727a7f526fcf95fd15f55b17f7d2d
SJCMN <sam.clavette@gmail.com>
2022-03-09 11:59:50 -0600

    adding validation, formArray, formBuilder

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-card-form',
@@ -7,19 +7,64 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./card-form.component.css'],
 })
 export class CardFormComponent implements OnInit {
-  cardForm = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-  });
-  
-get name(): any{
-  return this.cardForm.get('name');
-}
+  // individual FormGroup and FormControls
+  // cardForm = new FormGroup({
+  //   name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+  //   friendsName: new FormControl('', [
+  //     Validators.required,
+  //     Validators.minLength(3),
+  //   ]),
+  // });
 
-  constructor() {
-    console.log(this.cardForm.get('name'))
+   cardForm = this.fb.group({
+     name: ['', [Validators.required, Validators.minLength(3)]],
+     friendsName: [''],
+     address: this.fb.group({
+       street: [''],
+       city: [''],
+       state: [''],
+       zip: [''],
+     }),
+     aliases: this.fb.array([
+       this.fb.control('')
+     ])
+   });
+
+  onSubmit() {
+    console.log(this.cardForm.value);
   }
 
-  ngOnInit(): void {
-
+  updateCard() {
+    this.cardForm.patchValue({
+      name: 'Mother',
+      friendsName: 'Pants',
+    });
   }
+
+  clearCard() {
+    this.cardForm.patchValue({
+      name: '',
+      friendsName: '',
+      address: ({
+        street: '',
+        city: '',
+        state: '',
+        zip: ''
+      })
+    });
+  }
+
+  get aliases() {
+    return this.cardForm.get('aliases') as FormArray;
+  }
+
+  addAlias() {
+    this.aliases.push(this.fb.control(''));
+  }
+
+  constructor(private fb: FormBuilder) {
+   
+  }
+
+  ngOnInit(): void {}
 }
